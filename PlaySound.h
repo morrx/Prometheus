4cf04427ac88fb2bf9603d3cd9a2bb7d62f06445
morrx <38334478+morrx@users.noreply.github.com>
2018-04-23 17:20:35 -0500

    Update PlaySound.h

--- PlaySound.h
+++ PlaySound.h
@@ -2,16 +2,9 @@
 #include "HookIncludes.h"
 typedef void(__thiscall* play_sound_t)(void*, const char*);
 
-void IsReadyaccept()
-{
-	using IsReadyFn = void(__cdecl*)();
-	static auto fnIsReady = (void(__cdecl*)(void))U::pattern_scan(GetModuleHandleW(L"client.dll"), "55 8B EC 83 E4 F8 83 EC 08 56 8B 35 ? ? ? ? 57 83 BE");
-
-	reinterpret_cast<IsReadyFn>(fnIsReady)();
-}
-
 void __stdcall hkPlaySound(const char* szFileName)
 {
+
 	static auto ofunc = hooks::surface.get_original<play_sound_t>(82);
 	//Call original PlaySound
 	ofunc(g_Surface, szFileName);
@@ -20,17 +13,16 @@ void __stdcall hkPlaySound(const char* szFileName)
 
 	if (g_Options.Misc.AutoAccept)
 	{
-		if (!strstr(szFileName, "!UI/competitive_accept_beep.wav"))
-		{
-			IsReadyaccept();
 
-			FLASHWINFO flash;
-			flash.cbSize = sizeof(FLASHWINFO);
-			flash.hwnd = window;
-			flash.dwFlags = FLASHW_ALL | FLASHW_TIMERNOFG;
-			flash.uCount = 0;
-			flash.dwTimeout = 0;
-			FlashWindowEx(&flash);
+		if (strstr(szFileName, "UI/competitive_accept_beep.wav"))
+		{
+
+			static auto CServerConfirmedReservationCheckCallback__IsReady
+				= (void(__cdecl*)(void))U::pattern_scan(
+					GetModuleHandleW(L"client.dll"),
+					"55 8B EC 83 E4 F8 83 EC 08 56 8B 35 ? ? ? ? 57 83 BE"
+				);
+			CServerConfirmedReservationCheckCallback__IsReady();
 		}
 	}
 }
